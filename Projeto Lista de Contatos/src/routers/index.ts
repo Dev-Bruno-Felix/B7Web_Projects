f4144edf9c43ae7ee3134769e8ec4e1e5099d4dc
Dev-Bruno-Felix <dev.bruno.felix@outlook.com>
2025-07-09 14:05:43 -0300

    Project finish

--- Projeto Lista de Contatos/src/routers/index.ts
+++ Projeto Lista de Contatos/src/routers/index.ts
@@ -1,59 +1,30 @@
 import express from 'express';
-import { readFile, writeFile } from 'fs/promises';
-
-const dataSource = "./data/list.txt"
+import { createContact, deleteContact, getContacts } from '../services/contacts';
 
 const router = express.Router();
 
 router.post('/contato', async (req, res) => {
     const { name } = req.body;
-
     if (!name || name.length < 2) {
         return res.json({
             error: 'Name is required'
         });
     }
-
-    let list: string[] = [];
-    try {
-        const data = await readFile(dataSource, {encoding: 'utf8'});
-        list = data.split('\n');
-    } catch (err) { }
-
-    list.push(name);
-    await writeFile(dataSource, list.join('\n'));
-
+    await createContact(name as string)
     res.status(201).json({ contato: name })
 });
 
 router.get('/contatos', async (req, res) => {
-    let list: string[] = [];
-
-    try {
-        const data = await readFile(dataSource, {encoding: 'utf8'});
-        list = data.split('\n');
-    } catch (err) { }
-
+    let list = await getContacts()
     res.json({ contatos: list });
 });
 
 router.delete('/contato', async (req, res) => {
     const {name} = req.query;
-
     if (!name) {
         return res.json({error: 'Precisa mandar um nome para excluir.'});
     }
-
-    let list: string[] = [];
-    try {
-        const data = await readFile(dataSource, {encoding:'utf8'});
-        list = data.split('\n');
-    } catch (err) { }
-
-    list = list.filter(item => item.toLowerCase() !== (name as string).toLowerCase()); //Se for igual ele retira da nova lista
-
-    await writeFile(dataSource, list.join('\n'));
-
+    await deleteContact(name as string);
     res.json({contato: name});
 });
 
